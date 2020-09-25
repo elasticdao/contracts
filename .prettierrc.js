ca73362dbf7f060170212103b152168a459d38f8
smalldutta <smalldutta@gmail.com>
2020-09-25 14:38:48 +0300

    fixes linter and prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,5 @@ module.exports = {
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
+  endOfLine: 'lf',
 };
--- deploy/elasticDAO.js
+++ deploy/elasticDAO.js
@@ -11,21 +11,21 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
       ['Elastic DAO', 'Elastic Governance Token', 'EGT'],
       [true, true, false, true],
       [
-        ethers.BigNumber.from('100000000000000000000'), //k
-        ethers.BigNumber.from('100000000000000000'), //capitalDelta
-        ethers.BigNumber.from('20000000000000000'), //elasticity
-        ethers.BigNumber.from('1000000000000000000'), //initialShares
-        ethers.BigNumber.from('600000000000000000'), //approval
-        ethers.BigNumber.from('1000000000000000000'), //maxSharesPerAccount
-        ethers.BigNumber.from('120'), //contractVoteTypeMinBlocks
-        ethers.BigNumber.from('180'), //financeVoteTypeMinBlocks
-        ethers.BigNumber.from('60'), //informationVoteTypeMinBlocks
-        ethers.BigNumber.from('240'), //minBlocksForPenalty
-        ethers.BigNumber.from('90'), //permissionVoteTypeMinBlocks
-        ethers.BigNumber.from('1000000000000000000'), //minSharesToCreate
-        ethers.BigNumber.from('50000000000000000'), //penalty
-        ethers.BigNumber.from('500000000000000000'), //quorum
-        ethers.BigNumber.from('100000000000000000'), //reward
+        ethers.BigNumber.from('100000000000000000000'), // k
+        ethers.BigNumber.from('100000000000000000'), // capitalDelta
+        ethers.BigNumber.from('20000000000000000'), // elasticity
+        ethers.BigNumber.from('1000000000000000000'), // initialShares
+        ethers.BigNumber.from('600000000000000000'), // approval
+        ethers.BigNumber.from('1000000000000000000'), // maxSharesPerAccount
+        ethers.BigNumber.from('120'), // contractVoteTypeMinBlocks
+        ethers.BigNumber.from('180'), // financeVoteTypeMinBlocks
+        ethers.BigNumber.from('60'), // informationVoteTypeMinBlocks
+        ethers.BigNumber.from('240'), // minBlocksForPenalty
+        ethers.BigNumber.from('90'), // permissionVoteTypeMinBlocks
+        ethers.BigNumber.from('1000000000000000000'), // minSharesToCreate
+        ethers.BigNumber.from('50000000000000000'), // penalty
+        ethers.BigNumber.from('500000000000000000'), // quorum
+        ethers.BigNumber.from('100000000000000000'), // reward
       ],
     ],
   });
