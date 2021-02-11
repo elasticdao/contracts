c8e132cdba2b3dc77641481997d9266804a76223
alleycatdev <alleycatdev@protonmail.com>
2021-02-11 09:30:11 -0500

    last little tweak to transferFrom

--- src/tokens/ElasticGovernanceToken.sol
+++ src/tokens/ElasticGovernanceToken.sol
@@ -317,13 +317,13 @@ contract ElasticGovernanceToken is IElasticToken, ReentryProtection {
   ) external override preventReentry returns (bool) {
     require(msg.sender == _from || _amount <= _allowances[_from][msg.sender], 'ERC20: Bad Caller');
 
+
     if (msg.sender != _from && _allowances[_from][msg.sender] != uint256(-1)) {
       _allowances[_from][msg.sender] = SafeMath.sub(_allowances[_from][msg.sender], _amount);
+      emit Approval(msg.sender, _to, _allowances[_from][msg.sender]);
     }
 
     _transfer(_from, _to, _amount);
-    emit Approval(msg.sender, _to, _allowances[_from][msg.sender]);
-
     return true;
   }
 
