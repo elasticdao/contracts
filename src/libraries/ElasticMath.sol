219ade1574f7bb4d9bd719b636ac8de3ff2b3ba9
Dan Matthews <dan@cryptobuilders.io>
2021-02-19 15:59:17 +0200

    documentation for ElasticMath

--- src/libraries/ElasticMath.sol
+++ src/libraries/ElasticMath.sol
@@ -4,30 +4,50 @@ pragma solidity 0.7.2;
 import './SafeMath.sol';
 
 /**
- * This library does the Elastic math
+ * @dev Provides functions for performing ElasticDAO specific math.
+ *
+ * These functions correspond with functions provided by the JS SDK and should
+ * always be used instead of doing calculations within other contracts to avoid
+ * any inconsistencies in the math.
+ *
+ * Notes:
+ *
+ * - Dash values represent the state after a transaction has completed successfully.
+ * - Non-dash values represent the current state, before the transaction has completed.
+ * - Lambda is the math term for shares. We typically expose the value to users as
+ *   shares instead of lambda because it's easier to grok.
  */
-
 library ElasticMath {
+  /**
+   * @dev calculates the value of capitalDelta; the amount of ETH backing each
+   * governance token.
+   * @param totalEthValue amount of ETH in the DAO contract
+   * @param totalSupplyOfTokens number of tokens in existance
+   *
+   * capitalDelta = totalEthValue / totalSupplyOfTokens
+   * @return uint256
+   */
   function capitalDelta(uint256 totalEthValue, uint256 totalSupplyOfTokens)
     internal
     pure
     returns (uint256)
   {
-    return (wdiv(totalEthValue, totalSupplyOfTokens));
+    return wdiv(totalEthValue, totalSupplyOfTokens);
   }
 
   /**
-   * @dev calculates the value of deltaE
+   * @dev calculates the value of deltaE; the amount of ETH required to mint deltaLambda
    * @param deltaLambda = lambdaDash - lambda
-   * @param capitalDeltaValue is the Eth/Egt ratio
-   * @param k is a constant, initially set by the DAO
-   * @param elasticity is the value of elasticity, initially set by the DAO
-   * @param lambda = Current outstanding shares
-   * @param m = Current share modifier
+   * @param capitalDeltaValue the ETH/token ratio; see capitalDelta(uint256, uint256)
+   * @param k a constant, initially set by the DAO
+   * @param elasticity the percentage by which capitalDelta should increase
+   * @param lambda outstanding shares
+   * @param m share modifier
    *
+   * lambdaDash = deltaLambda + lambda
    * mDash = ( lambdaDash / lambda ) * m
-   * deltaE =  ( capitalDelta * k ( ( lambdaDash * mDash * ( 1 + elasticity ) ) - lambda * m )
-   * @return deltaEValue uint256
+   * deltaE = capitalDelta * k * ( lambdaDash * mDash * ( 1 + elasticity ) - lambda * m )
+   * @return uint256
    */
   function deltaE(
     uint256 deltaLambda,
@@ -36,7 +56,7 @@ library ElasticMath {
     uint256 elasticity,
     uint256 lambda,
     uint256 m
-  ) internal pure returns (uint256 deltaEValue) {
+  ) internal pure returns (uint256) {
     uint256 lambdaDash = SafeMath.add(deltaLambda, lambda);
 
     return
@@ -49,78 +69,89 @@ library ElasticMath {
       );
   }
 
+  /**
+   * @dev calculates the lambda value given t, k, & m
+   * @param tokens t value; number of tokens for which lambda should be calculated
+   * @param k a constant, initially set by the DAO
+   * @param m share modifier
+   *
+   * lambda = k * m / t
+   * @return uint256
+   */
   function lambdaFromT(
     uint256 tokens,
     uint256 k,
     uint256 m
-  ) internal pure returns (uint256 lambda) {
+  ) internal pure returns (uint256) {
     return wdiv(tokens, wmul(k, m));
   }
 
   /**
-   * @dev returns the value of mDash
-   * mDash = New share modifier
-   * @param m = Current share modifier
-   * @param lambda = Current outstanding shares
-   * @param lambdaDash = New outstanding shares
+   * @dev calculates the future share modifier given the future value of
+   * lambda (lambdaDash), the current value of lambda, and the current share modifier
+   * @param m current share modifier
+   * @param lambda current outstanding shares
+   * @param lambdaDash future outstanding shares
    *
    * mDash = ( lambdaDash / lambda ) * m
-   * @return mDashValue uint256
+   * @return uint256
    */
   function mDash(
     uint256 lambdaDash,
     uint256 lambda,
     uint256 m
-  ) internal pure returns (uint256 mDashValue) {
+  ) internal pure returns (uint256) {
     return wmul(wdiv(lambdaDash, lambda), m);
   }
 
   /**
-   * @dev returns the value of revamp
-   * @param elasticity is the value of elasticity initially set by the DAO
-   * upto 18 decimal points of precision
+   * @dev calculates the value of revamp
+   * @param elasticity the percentage by which capitalDelta should increase
    *
-   * Essentially takes in the value of elasticity and
-   * and returns the value of ( 1 + elasticity ) with 18 decimals of precision
-   * @return revampValue uint256
+   * revamp = 1 + elasticity
+   * @return uint256
    */
-  function revamp(uint256 elasticity) internal pure returns (uint256 revampValue) {
+  function revamp(uint256 elasticity) internal pure returns (uint256) {
     return SafeMath.add(elasticity, SafeMath.pow(10, 18));
   }
 
   /**
-   * @dev returns the value of the total tokens in the DAO
-   * @param lambda = Current outstanding shares
-   * @param k is a constant, initially set by the DAO
-   * @param m = Current share modifier
+   * @dev calculates the number of tokens represented by lambda given k & m
+   * @param lambda shares
+   * @param k a constant, initially set by the DAO
+   * @param m share modifier
    *
-   * t = ( lambda * m * k )
-   * @return tokens uint256
+   * t = lambda * m * k
+   * @return uint256
    */
   function t(
     uint256 lambda,
     uint256 k,
     uint256 m
-  ) internal pure returns (uint256 tokens) {
+  ) internal pure returns (uint256) {
     return wmul(wmul(lambda, k), m);
   }
 
   /**
-   * @dev multiplies two float values,
-   * required since soldity does not handle floating point values
+   * @dev multiplies two float values, required since solidity does not handle
+   * floating point values
+   *
+   * inspiration: https://github.com/dapphub/ds-math/blob/master/src/math.sol
+   *
    * @return uint256
    */
-  // inspiration: https://github.com/dapphub/ds-math/blob/master/src/math.sol
   function wmul(uint256 a, uint256 b) internal pure returns (uint256) {
     return SafeMath.add(SafeMath.mul(a, b), 1000000000000000000 / 2) / 1000000000000000000;
   }
 
   /**
-   * @dev divides two float values,
-   * required since soldity does not handle floating point values
+   * @dev divides two float values, required since solidity does not handle
+   * floating point values.
+   *
+   * inspiration: https://github.com/dapphub/ds-math/blob/master/src/math.sol
+   *
    * @return uint256
    */
-  // inspiration: https://github.com/dapphub/ds-math/blob/master/src/math.sol
   function wdiv(uint256 a, uint256 b) internal pure returns (uint256) {
     return SafeMath.add(SafeMath.mul(a, 1000000000000000000), b / 2) / b;
   }
